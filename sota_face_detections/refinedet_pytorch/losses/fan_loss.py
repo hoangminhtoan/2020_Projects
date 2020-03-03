a150084dec4a5d55ed2181a8cdd5c9e8be5f2993
hoangminhtoan <hoangminhtoant2l1@gmail.com>
2020-03-04 08:54:31 +0900

    update code

--- sota_face_detections/refinedet_pytorch/losses/fan_loss.py
+++ sota_face_detections/refinedet_pytorch/losses/fan_loss.py
@@ -25,73 +25,75 @@ def calc_iou(a, b):
 
     return IoU
 
-class AttentionLoss(nn.Module):
-    def forward(self, image_batch_shape, attention_mask, annotations):
-        h, w = image_batch_shape[2], image_batch_shape[3]
+class Attentionloss(nn.Module):
 
-        attention_losses = []
+    def forward(self, img_batch_shape, attention_mask, bboxs):
 
-        batch_size = annotations.shape[0]
+        h, w = img_batch_shape[2], img_batch_shape[3]
 
+        mask_losses = []
+
+        batch_size = bboxs.shape[0]
         for j in range(batch_size):
-            annotation = annotations[j, :, :]
-            annotation = annotation[annotation[:, 4] != -1]
 
-            cond1 = torch.le(annotation[:, 0], w)
-            cond2 = torch.le(annotation[:, 1], h)
-            cond3 = torch.le(annotation[:, 2], w)
-            cond4 = torch.le(annotation[:, 3], h)
+            bbox_annotation = bboxs[j, :, :]
+            bbox_annotation = bbox_annotation[bbox_annotation[:, 4] != -1]
+
+            cond1 = torch.le(bbox_annotation[:, 0], w)
+            cond2 = torch.le(bbox_annotation[:, 1], h)
+            cond3 = torch.le(bbox_annotation[:, 2], w)
+            cond4 = torch.le(bbox_annotation[:, 3], h)
             cond = cond1 * cond2 * cond3 * cond4
 
-            annotation = annotation[cond, :]
+            bbox_annotation = bbox_annotation[cond, :]
 
-            if annotation.shape[0] == 0:
-                if torch.cuda.is_available():
-                    attention_losses.append(torch.tensor(0).float().cuda())
-                else:
-                    attention_losses.append(torch.tensor(0).float())
+            if bbox_annotation.shape[0] == 0:
+                mask_losses.append(torch.tensor(0).float().cuda())
                 continue
-            
-            area = (annotation[:, 2] - annotation[:, 0]) * (annotation[:, 3] - annotation[:, 1])
 
-            attention_loss = []
+            bbox_area = (bbox_annotation[:, 2] - bbox_annotation[:, 0]) * (bbox_annotation[:, 3] - bbox_annotation[:, 1])
+
+            mask_loss = []
             for id in range(len(attention_mask)):
+
                 attention_map = attention_mask[id][j, 0, :, :]
 
-                min_area = (2 ** (id+ 5)) ** 2 * 0.5
+                min_area = (2 ** (id + 5)) ** 2 * 0.5
                 max_area = (2 ** (id + 5) * 1.58) ** 2 * 2
 
-                indice1 = torch.ge(area, min_area)
-                indice2 = torch.le(area, max_area) 
-                indice = indice1 * indice2
+                level_bbox_indice1 = torch.ge(bbox_area, min_area)
+                level_bbox_indice2 = torch.le(bbox_area, max_area)
 
-                level_annotation = annotation[indice, :].clone()
+                level_bbox_indice = level_bbox_indice1 * level_bbox_indice2
 
-                attention_h, attention_w = attention_map.shape 
+                level_bbox_annotation = bbox_annotation[level_bbox_indice, :].clone()
 
-                if level_annotation.shape[0]:
-                    level_annotation[:, 0] *= attention_w / w
-                    level_annotation[:, 1] *= attention_h / h
-                    level_annotation[:, 2] *= attention_w / w
-                    level_annotation[:, 3] *= attention_h / h
-                
-                gt_mask = torch.zeros(attention_map.shape)
+                #level_bbox_annotation = bbox_annotation.clone()
 
-                if torch.cuda.is_available():
-                    gt_mask = gt_mask.cuda()
-                
-                for i in range(level_annotation.shape[0]):
-                    x1 = max(int(level_annotation[i, 0]), 0)
-                    y1 = max(int(level_annotation[i, 1]), 0)
-                    x2 = min(math.ceil(level_annotation[i, 2]) + 1, attention_w)
-                    y2 = min(math.ceil(level_annotation[i, 3]) + 1, attention_h)
+                attention_h, attention_w = attention_map.shape
 
-                    gt_mask[y1:y2, x1:x2] = 1
-                
-                gt_mask = gt_mask[gt_mask >= 0]
-                predicti_mask = attention_map[attention_map >= 0]
+                if level_bbox_annotation.shape[0]:
+                    level_bbox_annotation[:, 0] *= attention_w / w
+                    level_bbox_annotation[:, 1] *= attention_h / h
+                    level_bbox_annotation[:, 2] *= attention_w / w
+                    level_bbox_annotation[:, 3] *= attention_h / h
 
-                attention_loss.append(F.binary_cross_entropy(predicti_mask, gt_mask))
-            attention_losses.append(torch.stack(attention_loss).mean())
+                mask_gt = torch.zeros(attention_map.shape)
+                mask_gt = mask_gt.cuda()
 
-        return torch.stack(attention_losses).mean(dim=0, keepdim=True)
+                for i in range(level_bbox_annotation.shape[0]):
+
+                    x1 = max(int(level_bbox_annotation[i, 0]), 0)
+                    y1 = max(int(level_bbox_annotation[i, 1]), 0)
+                    x2 = min(math.ceil(level_bbox_annotation[i, 2]) + 1, attention_w)
+                    y2 = min(math.ceil(level_bbox_annotation[i, 3]) + 1, attention_h)
+
+                    mask_gt[y1:y2, x1:x2] = 1
+
+                mask_gt = mask_gt[mask_gt >= 0]
+                mask_predict = attention_map[attention_map >= 0]
+
+                mask_loss.append(F.binary_cross_entropy(mask_predict, mask_gt))
+            mask_losses.append(torch.stack(mask_loss).mean())
+
+        return torch.stack(mask_losses).mean(dim=0, keepdim=True)
