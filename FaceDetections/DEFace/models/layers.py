636841de21879323eda69a9ad1bbaf5f69027e95
toanmh <hoangminhtoant2l1@yahoo.com>
2020-04-30 13:45:10 +0900

    Add code

---
+++ FaceDetections/DEFace/models/layers.py
@@ -0,0 +1,312 @@
+import torch
+import torch.nn as nn
+import numpy as np
+
+# Squeeze and Excitation Module
+
+
+class SELayer(nn.Module):
+    def __init__(self, channels, reduction=16):
+        super(SELayer, self).__init__()
+
+        self.avg_pool = nn.AdaptiveAvgPool2d(1)
+        self.fc = nn.Sequential(
+            nn.Conv2D(channels, channels // reduction,
+                      kernel_size=1, padding=0),
+            nn.ReLU(inplace=True),
+            nn.Conv2D(channels, channels // reduction,
+                      kernel_size=1, padding=0),
+            nn.Sigmoid()
+        )
+
+    def forward(self, x):
+        b, c, _, _ = x.size()  # batch, channel, height, width
+
+        out = self.avg_pool(x).view(b, c)
+        out = self.fc(out).view(b, c, 1, 1)
+
+        out = x * out.expand_as(x)
+
+        return out
+
+# Bottle Attention Module
+
+
+class BAMLayer(nn.Module):
+    def __init__(self, channels, reduction=16):
+        super(BAMLayer, self).__init__()
+
+        self.avg_pool = nn.AdaptiveAvgPool2d(1)
+        self.max_pool = nn.AdaptiveMaxPool2d(1)
+
+        self.fc = nn.Sequential(
+            nn.Conv2d(channels, channels // reduction,
+                      kernel_size=1, padding=0),
+            nn.ReLU(inplace=True),
+            nn.Conv2d(channels // reduction, channels,
+                      kernel_size=1, padding=0),
+            nn.Sigmoid()
+        )
+
+        self.conv_after_concat = nn.Conv2d(
+            2, 1, kernel_size=7, stride=1, padding=3)
+
+    def forward(self, x):
+        residul = x
+
+
+class BasicBlock(nn.Module):
+    expansion = 1
+
+    def __init__(self, in_planes, out_planes, stride=1, downsample=None):
+        super(BasicBloc, self).__init__()
+
+        self.conv1 = nn.Conv2d(
+            in_planes, out_planes, kernel_size=3, stride=stride, padding=1, bias=False)
+        self.conv2 = nn.Conv2d(
+            out_planes, out_planes, kernel_size=3, stride=stride, padding=1, bias=False)
+        self.bn = nn.BatchNorm2d(out_planes)
+        self.relu = nn.ReLU(inplace=True)
+        self.downsample = downsample
+        self.stride = stride
+
+    def forward(self, x):
+        residual = x
+
+        out = self.conv1(x)
+        out = self.bn(out)
+        out = self.relu(out)
+
+        out = self.conv2(out)
+        out = self.bn(out)
+
+        if self.downsample is not None:
+            residual = self.downsample(x)
+
+        out += residual
+        out = self.relu(out)
+
+        return out
+
+
+class BottleneckBlock(nn.Module):
+    expansion = 4
+
+    def __init__(self, in_planes, out_planes, stride=1, downsample=None):
+        super(BottleneckBlock, self).__init__()
+
+        self.conv1 = nn.Conv2d(in_planes, out_planes,
+                               kernel_size=1, bias=False)
+        self.bn1 = nn.BatchNorm2d(out_planes)
+        self.conv2 = nn.Conv2d(
+            out_planes, out_planes, kernel_size=3, stride=stride, padding=1, bias=False)
+        self.bn2 = nn.BatchNorm2d(out_planes)
+        self.conv3 = nn.Conv2d(out_planes, out_planes *
+                               4, kernel_size=1, bias=False)
+        self.bn3 = nn.BatchNorm2d(out_planes * 4)
+        self.relu = nn.ReLU(inplace=True)
+        self.stride = stride
+        self.downsample = downsample
+
+    def forward(self, x):
+        residual = x
+
+        out = self.conv1(x)
+        out = self.bn1(out)
+        out = self.relu(out)
+
+        out = self.conv2(out)
+        out = self.bn2(out)
+        out = self.relu(out)
+
+        out = self.conv3(out)
+        out = self.bn3(out)
+
+        if self.downsample is not None:
+            residual = self.downsample(x)
+
+        out += residual
+        out = self.relu(out)
+
+        return out
+
+
+class BasicSEBlock(nn.Module):
+    expansion = 1
+
+    def __init__(self, in_planes, out_planes, stride=1, downsample=None, reduction=16):
+        super(BasicSEBlock, self).__init__()
+
+        self.conv1 = nn.Conv2d(
+            in_planes, out_planes, kernel_size=3, stride=stride, padding=1, bias=False)
+        self.conv2 = nn.Conv2d(
+            in_planes, out_planes, kernel_size=3, stride=stride, padding=1, bias=False)
+        self.se = SELayer(out_planes, reduction=reduction)
+        self.bn = nn.BatchNorm2d(out_planes)
+        self.relu = nn.ReLU(inplace=True)
+        self.downsample = downsample
+        self.stride = stride
+
+    def forward(self, x):
+        residual = x
+
+        out = self.conv1(x)
+        out = self.bn(out)
+        out = self.relu(out)
+
+        out = self.conv2(out)
+        out = self.bn(out)
+        out = self.se(out)
+
+        if self.downsample is not None:
+            residual = self.downsample(x)
+
+        out += residual
+        out = self.relu(out)
+
+        return out
+
+
+class BottleneckSEBlock(nn.Module):
+    expansion = 4
+
+    def __init__(self, in_planes, out_planes, stride=1, downsample=None, reduction=16):
+        super(BottleneckSEBlock, self).__init__()
+
+        self.conv1 = nn.Conv2d(in_planes, out_planes,
+                               kernel_size=1, bias=False)
+        self.bn1 = nn.BatchNorm2d(out_planes)
+        self.conv2 = nn.Conv2d(
+            out_planes, out_planes, kernel_size=3, stride=stride, padding=1, bias=False)
+        self.bn2 = nn.BatchNorm2d(out_planes)
+        self.conv3 = nn.Conv2d(out_planes, out_planes *
+                               4, kernel_size=1, bias=False)
+        self.bn3 = nn.BatchNorm2d(out_planes * 4)
+        self.se = SELayer(out_planes * 4, reduction=reduction)
+        self.relu = nn.ReLU(inplace=True)
+        self.downsample = downsample
+        self.stride = stride
+
+    def forward(self, x):
+        residual = x
+
+        out = self.conv1(x)
+        out = self.bn1(out)
+        out = self.relu(out)
+
+        out = self.conv2(out)
+        out = self.bn2(out)
+        out = self.relu(out)
+
+        out = self.conv3(out)
+        out = self.bn3(out)
+        out = self.se(out)
+
+        if self.downsample is not None:
+            residual = self.downsample(x)
+
+        out += residual
+        out = self.relu(out)
+
+        return out
+
+# Additional Attention Context Module
+
+
+class ContextModule(nn.Module):
+    def __init(self, in_planes, out_planes=256):
+        super(ContextModule, self).__init__()
+
+        self.conv1 = nn.Conv2d(in_planes, out_planes, kernel_size=3, padding=1)
+        self.conv2 = nn.Conv2d(out_planes, out_planes,
+                               kernel_size=3, padding=1)
+        self.conv3 = nn.Conv2d(out_planes, out_planes,
+                               kernel_size=3, padding=1)
+        self.conv4 = nn.Conv2d(out_planes, out_planes,
+                               kernel_size=3, padding=1)
+        self.conv5 = nn.Conv2d(out_planes, 1, kernel_size=3, padding=1)
+        self.bn5 = nn.BatchNorm2d(1)
+        self.bn = nn.BatchNorm2d(out_planes)
+        self.relu = nn.ReLU(inplace=True)
+        self.sigmoid = nn.Sigmoid()
+
+    def forward(self, x):
+        out = self.conv1(x)
+        out = self.bn(out)
+        out = self.relu(out)
+
+        out = self.conv2(out)
+        out = self.bn(out)
+        out = self.relu(out)
+
+        out = self.conv3(out)
+        out = self.bn(out)
+        out = self.relu(out)
+
+        out = self.conv4(out)
+        out = self.bn(out)
+        out = self.relu(out)
+
+        out = self.conv5(out)
+        out = self.bn(out)
+        out = self.sigmoid(out)
+
+        return out
+
+
+class BBoxTransform(nn.Module):
+    def __init__(self, mean=None, std=None):
+        super(BBoxTransform, self).__init__()
+
+        if mean is None:
+            self.mean = torch.from_numpy(
+                np.array([0, 0, 0, 0]).astype(np.float32)).cuda()
+        else:
+            self.mean = mean
+
+        if std is None:
+            self.std = torch.from_numpy(
+                np.array([0.1, 0.1, 0.2, 0.2]).astype(np.float32)).cuda()
+        else:
+            self.std = std
+
+    def forward(self, boxes, deltas):
+        widths = boxes[:, :, 2] - boxes[:, :, 0]
+        heights = boxes[:, :, 3] - boxes[:, :, 1]
+        ctr_x = boxes[:, :, 0] + 0.5 * widths
+        ctr_y = boxes[:, :, 1] + 0.5 * heights
+
+        dx = deltas[:, :, 0] * self.std[0] + self.mean[0]
+        dy = deltas[:, :, 1] * self.std[1] + self.mean[1]
+        dw = deltas[:, :, 2] * self.std[2] + self.mean[2]
+        dh = deltas[:, :, 3] * self.std[3] + self.mean[3]
+
+        pred_ctr_x = ctr_x + dx * widths
+        pred_ctr_y = ctr_y + dy * heights
+        pred_w = torch.exp(dw) * widths
+        pred_h = torch.exp(dh) * heights
+
+        pred_boxes_x1 = pred_ctr_x - 0.5 * pred_w
+        pred_boxes_y1 = pred_ctr_y - 0.5 * pred_h
+        pred_boxes_x2 = pred_ctr_x + 0.5 * pred_w
+        pred_boxes_y2 = pred_ctr_y + 0.5 * pred_h
+
+        pred_boxes = torch.stack(
+            [pred_boxes_x1, pred_boxes_y1, pred_boxes_x2, pred_boxes_y2], dim=2)
+
+        return pred_boxes
+
+
+class ClipBoxes(nn.Module):
+    def __init__(self, width=None, height=None):
+        super(ClipBoxes, self).__init__()
+
+    def forward(self, boxes, img):
+        batch_size, channels, height, width = img.shape
+
+        boxes[:, :, 0] = torch.clamp(boxes[:, :, 0], min=0)
+        boxes[:, :, 1] = torch.clamp(boxes[:, :, 1], min=0)
+        boxes[:, :, 2] = torch.clamp(boxes[:, :, 2], max=width)
+        boxes[:, :, 3] = torch.clamp(boxes[:, :, 3], max=height)
+
+        return boxes
