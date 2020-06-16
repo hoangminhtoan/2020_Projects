a2ad3aebcd50e4dae5e5729de0f716ac90909c0b
Toan Hoang <hoangminhtoant2l1@yahoo.com>
2020-06-16 23:41:35 +0900

    Update

---
+++ Swift/HelloWorld.playground/Contents.swift
@@ -0,0 +1,18 @@
+import UIKit
+
+var str = "Hello, playground"
+
+// create list
+let names = ["Anna", "Alex", "Brian", "Jack"]
+let count = names.count
+
+/*
+for i in 0..<coun{
+    print("Person \(i+1) is called \(names[i])\n")
+}
+*/
+
+for name in names{
+    print(name)
+}
+
