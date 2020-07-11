5c274bc6e4b64a9360ca8bcdd1fff95190a7cde7
Toan Hoang <hoangminhtoant2l1@yahoo.com>
2020-07-11 23:46:48 +0900

    Commit Code

--- Swift/Playground/DataStructure&Algorithms/SwiftAlgorithms.playground/Contents.swift
+++ Swift/Playground/DataStructure&Algorithms/SwiftAlgorithms.playground/Contents.swift
@@ -1,6 +1,6 @@
 import UIKit
 
-example(of: "Createing and linking nodes"){
+example(of: "Creating and Linking Nodes"){
     let node1 = Node(value: 1)
     let node2 = Node(value: 2)
     let node3 = Node(value: 3)
@@ -9,4 +9,74 @@ example(of: "Createing and linking nodes"){
     node2.next = node3
     
     print(node1)
+}
+
+example(of: "Pushing Element into the List"){
+    var list = LinkedList<Int>()
+    list.push(3)
+    list.push(2)
+    list.push(1)
+    
+    print(list)
+}
+
+
+example(of: "Appending Element into the List"){
+    var list = LinkedList<Int>()
+    list.append(3)
+    list.append(2)
+    list.append(1)
+    
+    print(list)
+}
+
+example(of: "Inserting at a Particular Index"){
+    var list = LinkedList<Int>()
+    list.push(3)
+    list.push(2)
+    list.append(1)
+    
+    print("Before Inserting: \(list)")
+    var middleNode = list.node(at: 1)!
+    for i in 1...4{
+        middleNode = list.insert(i*2, after: middleNode)
+    }
+    print("After Inserting: \(list)")
+}
+
+example(of: "Removing Head Node of Linked List"){
+    var list = LinkedList<Int>()
+    for i in 1...5{
+        list.push(i)
+    }
+    
+    print("Before Popping List: \(list)")
+    
+    list.pop()
+    print("After Popping List: \(list)")
+}
+
+example(of: "Removing Last Node of Linked List"){
+    var list = LinkedList<Int>()
+    for i in 1...5{
+        list.push(i)
+    }
+    
+    print("Before remove The Last Node: \(list)")
+    
+    list.removeLast()
+    print("After Removing the Last Node: \(list)")
+}
+
+
+example(of: "Using Collection"){
+    var list = LinkedList<Int>()
+    
+    for i in 0...9{
+        list.append(i)
+    }
+    
+    print("List: \(list)")
+    print("First Element: \(list[list.startIndex])")
+    
 }
--- Swift/Playground/DataStructure&Algorithms/SwiftAlgorithms.playground/Sources/LinkList.swift
+++ Swift/Playground/DataStructure&Algorithms/SwiftAlgorithms.playground/Sources/LinkList.swift
@@ -19,3 +19,147 @@ extension Node: CustomStringConvertible{
         return "\(value) -> " + String(describing: next) + " "
     }
 }
+
+
+public struct LinkedList<Value> {
+    public var head: Node<Value>?
+    public var tail: Node<Value>?
+    
+    public init() {}
+    
+    public var isEmpty: Bool {
+        return head == nil
+    }
+    
+    // Insert at the head of linked list
+    public mutating func push(_ value: Value){
+        head = Node(value: value, next: head)
+        if tail == nil{
+            tail = head
+        }
+    }
+    
+    // Insert at the end of linked list
+    public mutating func append(_ value: Value){
+        guard !isEmpty else {
+            push(value)
+            return
+        }
+        
+        tail!.next = Node(value: value)
+        
+        tail = tail!.next
+    }
+    
+    // Insert at specific place of linked list
+    @discardableResult
+    public mutating func insert(_ value: Value, after node: Node<Value>) -> Node<Value> {
+        guard tail !== node else {
+            append(value)
+            return tail!
+        }
+        
+        node.next = Node(value: value, next: node.next)
+        return node.next!
+    }
+    
+    public func node(at index: Int) -> Node<Value>?{
+        var curr = head
+        var currIdx = 0
+        
+        while curr != nil && currIdx < index{
+            curr = curr!.next
+            currIdx += 1
+        }
+        
+        return curr
+    }
+    
+    // Remove head node
+    @discardableResult
+    public mutating func pop() -> Value? {
+        defer{
+            head = head?.next
+            if isEmpty{
+                tail = nil
+            }
+        }
+        return head?.value
+    }
+    
+    @discardableResult
+    public mutating func removeLast() -> Value? {
+        guard let head = head else {
+            return nil
+        }
+        
+        guard head.next != nil else{
+            return pop()
+        }
+        
+        var prev = head
+        var current = head
+        
+        while let next = current.next{
+            prev = current
+            current = next
+        }
+        
+        prev.next = nil
+        tail = prev
+        return current.value
+    }
+}
+
+extension LinkedList: CustomStringConvertible{
+    public var description: String {
+        guard let head = head else {
+            return "Empty List"
+        }
+        return String(describing: head)
+    }
+}
+
+extension LinkedList: Collection {
+    public struct Index: Comparable {
+        public var node: Node<Value>?
+        
+        static public func == (lhs: Index, rhs: Index) -> Bool {
+            switch (lhs.node, rhs.node){
+            case let (left?, right?):
+                return left.next === right.next
+            case (nil, nil):
+                return true
+            default:
+                return false
+            }
+        }
+        
+        static public func <(lhs: Index, rhs: Index) -> Bool {
+            guard  lhs != rhs else {
+                return false
+            }
+            
+            let nodes = sequence(first: lhs.node) {
+                $0?.next
+            }
+            return nodes.contains {$0 === rhs.node}
+        }
+    }
+    
+    public var startIndex: Index {
+        return Index(node: head)
+    }
+    
+    public var endIndex: Index {
+        return Index(node: tail?.next)
+    }
+    
+    public func index(after i: Index) -> Index {
+        return Index(node: i.node?.next)
+    }
+    
+    public subscript(position: Index) -> Value {
+        return position.node!.value
+    }
+}
