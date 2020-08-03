e3eaa0766072fdd9b8b9f5365e742e0c62b61211
hoangminhtoan <hoangminhtoant2l1@yahoo.com>
2020-08-03 17:00:30 +0900

    Update Code

--- GANs/GANsInAction/self-attention-gan/trainer.py
+++ GANs/GANsInAction/self-attention-gan/trainer.py
@@ -1,28 +1,29 @@
-import os 
-import time 
-import torch 
-import datetime 
+import os
+import time
+import torch
+import datetime
 
-import torch.nn as nn 
-from torch.autograd import Variable 
-from torchvision.utils import save_image 
+import torch.nn as nn
+from torch.autograd import Variable
+from torchvision.utils import save_image
 
-from sagan_models import Generator, Discriminator 
-from utils import * 
+from sagan_models import Generator, Discriminator
+from utils import *
 
 class Trainer(object):
     def __init__(self, data_loader, config):
+
         # Data loader
         self.data_loader = data_loader
 
-        # Exact model and loss
-        self.model = config.model 
-        self.adv_loss = config.adv_loss 
+        # exact model and loss
+        self.model = config.model
+        self.adv_loss = config.adv_loss
 
         # Model hyper-parameters
-        self.imsize = config.imsize 
-        self.g_num = config.g_num 
-        self.z_dim = config.z_dim 
+        self.imsize = config.imsize
+        self.g_num = config.g_num
+        self.z_dim = config.z_dim
         self.g_conv_dim = config.g_conv_dim
         self.d_conv_dim = config.d_conv_dim
         self.parallel = config.parallel
@@ -64,7 +65,10 @@ class Trainer(object):
         if self.pretrained_model:
             self.load_pretrained_model()
 
+
+
     def train(self):
+
         # Data iterator
         data_iter = iter(self.data_loader)
         step_per_epoch = len(self.data_loader)
@@ -82,7 +86,8 @@ class Trainer(object):
         # Start time
         start_time = time.time()
         for step in range(start, self.total_step):
-            # ========== Train D ========= #
+
+            # ================== Train D ================== #
             self.D.train()
             self.G.train()
 
@@ -92,39 +97,45 @@ class Trainer(object):
                 data_iter = iter(self.data_loader)
                 real_images, _ = next(data_iter)
 
-            # Computer loass with real images
+            # Compute loss with real images
+            # dr1, dr2, df1, df2, gf1, gf2 are attention scores
             real_images = tensor2var(real_images)
-            d_out_real, dr1, dr2 = self.D(real_images)
-            if self.adv_loss == 'wgan_gp':
+            d_out_real,dr1,dr2 = self.D(real_images)
+            if self.adv_loss == 'wgan-gp':
                 d_loss_real = - torch.mean(d_out_real)
             elif self.adv_loss == 'hinge':
-                d_loss_real = torch.nn.ReLU()(1. - d_out_real).mean()
+                d_loss_real = torch.nn.ReLU()(1.0 - d_out_real).mean()
 
             # apply Gumbel Softmax
             z = tensor2var(torch.randn(real_images.size(0), self.z_dim))
-            fake_images, gf1, gf2 = self.G(z)
-            d_out_fake, df1, df2 = self.D(fake_images)
+            fake_images,gf1,gf2 = self.G(z)
+            d_out_fake,df1,df2 = self.D(fake_images)
 
-            if self.adv_loss == 'wgan_gp':
+            if self.adv_loss == 'wgan-gp':
                 d_loss_fake = d_out_fake.mean()
             elif self.adv_loss == 'hinge':
-                d_loss_fake == torch.nn.ReLU()(1. + d_out_fake).mean()
+                d_loss_fake = torch.nn.ReLU()(1.0 + d_out_fake).mean()
+
 
             # Backward + Optimize
             d_loss = d_loss_real + d_loss_fake
             self.reset_grad()
-            d_loss.backward() 
-            self.d_optimizer.step() 
+            d_loss.backward()
+            self.d_optimizer.step()
 
-            if self.adv_loss == 'wgan_gp':
+
+            if self.adv_loss == 'wgan-gp':
                 # Compute gradient penalty
                 alpha = torch.rand(real_images.size(0), 1, 1, 1).cuda().expand_as(real_images)
-                interpolated = Variable(alpha * real_images.data + (1. - alpha) * fake_images.data, requires_grad=True)
-                out, _, _ = self.D(interpolated)
+                interpolated = Variable(alpha * real_images.data + (1 - alpha) * fake_images.data, requires_grad=True)
+                out,_,_ = self.D(interpolated)
 
-                grad = torch.autograd.grad(
-                    outputs=out, grad_outputs=torch.ones(out.size()).cuda(),
-                    retain_graph=True, create_graph=True, only_inputs=True)[0]
+                grad = torch.autograd.grad(outputs=out,
+                                           inputs=interpolated,
+                                           grad_outputs=torch.ones(out.size()).cuda(),
+                                           retain_graph=True,
+                                           create_graph=True,
+                                           only_inputs=True)[0]
 
                 grad = grad.view(grad.size(0), -1)
                 grad_l2norm = torch.sqrt(torch.sum(grad ** 2, dim=1))
@@ -137,22 +148,24 @@ class Trainer(object):
                 d_loss.backward()
                 self.d_optimizer.step()
 
-            # ========== Train G and gumbel ========= #
+            # ================== Train G and gumbel ================== #
             # Create random noise
             z = tensor2var(torch.randn(real_images.size(0), self.z_dim))
-            fake_images, _, _ = self.G(z)
+            fake_images,_,_ = self.G(z)
 
             # Compute loss with fake images
-            g_out_fake, _, _ = self.D(fake_images)
+            g_out_fake,_,_ = self.D(fake_images)  # batch x n
             if self.adv_loss == 'wgan-gp':
                 g_loss_fake = - g_out_fake.mean()
             elif self.adv_loss == 'hinge':
-                g_loss_fake = -g_out_fake.mean()
-            
+                g_loss_fake = - g_out_fake.mean()
+
             self.reset_grad()
             g_loss_fake.backward()
             self.g_optimizer.step()
 
+
+            # Print out log info
             if (step + 1) % self.log_step == 0:
                 elapsed = time.time() - start_time
                 elapsed = str(datetime.timedelta(seconds=elapsed))
@@ -161,44 +174,47 @@ class Trainer(object):
                       format(elapsed, step + 1, self.total_step, (step + 1),
                              self.total_step , d_loss_real.data[0],
                              self.G.attn1.gamma.mean().data[0], self.G.attn2.gamma.mean().data[0] ))
-            
+
             # Sample images
             if (step + 1) % self.sample_step == 0:
-                fake_images, _, _ = self.G(fixed_z)
-                save_image(denorm(fake_images.data), os.path.join(self.sample_path, '{}_fake.png'.format(step + 1)))
+                fake_images,_,_= self.G(fixed_z)
+                save_image(denorm(fake_images.data),
+                           os.path.join(self.sample_path, '{}_fake.png'.format(step + 1)))
 
-            if (step + 1) % model_save_step == 0:
-                torch.save(self.G.state_dict(), os.path.join(self.model_save_path, '{}_G.pth'.format(step + 1)))
-                torch.save(self.D.state_dict(), os.path.join(self.model_save_path, '{}_D.pth'.format(step + 1)))
-            
+            if (step+1) % model_save_step==0:
+                torch.save(self.G.state_dict(),
+                           os.path.join(self.model_save_path, '{}_G.pth'.format(step + 1)))
+                torch.save(self.D.state_dict(),
+                           os.path.join(self.model_save_path, '{}_D.pth'.format(step + 1)))
+
     def build_model(self):
-        self.G = Generator(self.batch_size, self.imsize, self.z_dim, self.g_conv_dim).cuda()
-        self.D = Discriminator(self.batch_size, self.imsize, self.d_conv_dim).cuda()
+
+        self.G = Generator(self.batch_size,self.imsize, self.z_dim, self.g_conv_dim).cuda()
+        self.D = Discriminator(self.batch_size,self.imsize, self.d_conv_dim).cuda()
         if self.parallel:
             self.G = nn.DataParallel(self.G)
             self.D = nn.DataParallel(self.D)
 
         # Loss and optimizer
+        # self.g_optimizer = torch.optim.Adam(self.G.parameters(), self.g_lr, [self.beta1, self.beta2])
         self.g_optimizer = torch.optim.Adam(filter(lambda p: p.requires_grad, self.G.parameters()), self.g_lr, [self.beta1, self.beta2])
         self.d_optimizer = torch.optim.Adam(filter(lambda p: p.requires_grad, self.D.parameters()), self.d_lr, [self.beta1, self.beta2])
 
         self.c_loss = torch.nn.CrossEntropyLoss()
+        # print networks
         print(self.G)
         print(self.D)
 
     def build_tensorboard(self):
         from logger import Logger
-
         self.logger = Logger(self.log_path)
 
     def load_pretrained_model(self):
         self.G.load_state_dict(torch.load(os.path.join(
             self.model_save_path, '{}_G.pth'.format(self.pretrained_model))))
-        
         self.D.load_state_dict(torch.load(os.path.join(
             self.model_save_path, '{}_D.pth'.format(self.pretrained_model))))
-
-        print('loaded trained models (step: {})...!'.format(self.pretrained_model))
+        print('loaded trained models (step: {})..!'.format(self.pretrained_model))
 
     def reset_grad(self):
         self.d_optimizer.zero_grad()
@@ -207,6 +223,3 @@ class Trainer(object):
     def save_sample(self, data_iter):
         real_images, _ = next(data_iter)
         save_image(denorm(real_images), os.path.join(self.sample_path, 'real.png'))
-
-
-
